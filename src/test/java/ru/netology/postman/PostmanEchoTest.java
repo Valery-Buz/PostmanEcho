4bb4b9f416bf9abded99af2aad61f6b705a6509e
Valery Buz <Ajven@yandex.ru>
2022-02-22 16:31:59 +0300

    initial commit

---
+++ src/test/java/ru/netology/postman/PostmanEchoTest.java
@@ -0,0 +1,26 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+
+}
